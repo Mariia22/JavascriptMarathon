d068f4b36fcb0a3abd728bc75a26b69cd0f59f99
Maria22 <m.ushakova22@gmail.com>
2020-10-22 16:36:07 +0200

    -add scrubs to video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,6 +2,8 @@ const video = document.querySelector('.viewer');
 const button = document.querySelector('.toggle');
 const skip = document.querySelectorAll('[data-skip]');
 const range = document.querySelectorAll('.player__slider');
+const progressFilled = document.querySelector('.progress__filled');
+const progress = document.querySelector('.progress');
 
 function toggleVideo() {
     let method = video.paused ? 'play' : 'pause';
@@ -21,10 +23,26 @@ function handleRange() {
     video[this.name] = this.value;
 }
 
+function updateProgress() {
+    let scrubTime = (video.currentTime / video.duration) * 100;
+    progressFilled.style.flexBasis = `${scrubTime}%`;
+
+}
+function handleVideo(e) {
+    let handleTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    console.log(handleTime);
+    video.currentTime = handleTime;
+}
+
 skip.forEach(buttonSkip => buttonSkip.addEventListener('click', skipVideo));
 range.forEach(buttonRange => buttonRange.addEventListener('mousemove', handleRange));
 
 video.addEventListener('click', toggleVideo);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', updateProgress);
 button.addEventListener('click', toggleVideo);
+let mousedown = false;
+progress.addEventListener('mousemove', (e) => mousedown && handleVideo(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
